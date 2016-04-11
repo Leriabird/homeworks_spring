71ba174ee323e7310173da7751abf9af72e28a89
Leriabird <leriapingui@gmail.com>
2016-04-11 08:34:05 +0200

    zalila
    perezalila

--- test1/test1.py
+++ test1/test1.py
@@ -8,13 +8,13 @@ import subprocess
 parser = argparse.ArgumentParser(description="two required args: percentage and path; one optional arg: path to new file")
 
 parser.add_argument("percentage",
-                    type=int,
+                    type=str,
                     help="% of compression")
 parser.add_argument("path",
                     type=str,
                     help="PATH to file/folder")
 parser.add_argument("newpath",
-                    type=int,
+                    type=str,
                     help="path to new file",
                     nargs='?')
 
@@ -27,14 +27,19 @@ newpath = args.newpath
 if os.path.isfile(path):
     if newpath:
         subcommand = "convert " + path + " -resize " + percentage + "% " + newpath # the callname passed to subprocess
-        print(subprocess.run(subcommand, stdin=None, input=None, stdout=None, shell=True, check=False))
+        subprocess.run(subcommand, stdin=None, input=None, stdout=None, shell=True, check=False)
     else:
         subcommand = "convert " + path + " -resize " + percentage + "% " + path # the callname passed to subprocess
         subprocess.run(subcommand, stdin=None, input=None, stdout=None, shell=True, check=False)
 elif os.path.isdir(path):
-    subcommand = "find . -iregex '.*.png' -exec convert '{}' -resize " + percentage + " png:'{}' \ "
+    subcommand = "find . -iregex '.*.jpg' -exec convert '{}' -resize " + percentage + " png:'{}' \;"
     subprocess.run(subcommand, stdin=None, input=None, stdout=None, shell=True, check=False)
-    subcommand = "find . -iregex '.*.jpg' -exec convert '{}' -resize " + percentage + " jpg:'{}' \ "
+    subcommand = "find . -iregex '.*.png' -exec convert '{}' -resize " + percentage + " jpg:'{}' \;"
     subprocess.run(subcommand, stdin=None, input=None, stdout=None, shell=True, check=False)
 else:
     print('Error: unsupported data type')
+
+    #.*.(png|jpg)
+
+
+    #.*\(deb\|vmdk\)$
